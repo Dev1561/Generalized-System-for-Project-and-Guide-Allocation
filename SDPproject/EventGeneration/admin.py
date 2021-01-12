7a1e0582f014f61e86a05e97628c2035387cafed
Jay <modi@Jays-MacBook-Air.local>
2021-01-12 16:29:52 +0530

    models created

--- SDPproject/EventGeneration/admin.py
+++ SDPproject/EventGeneration/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Student, Event
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Student)
+admin.site.register(Event)
--- SDPproject/EventGeneration/models.py
+++ SDPproject/EventGeneration/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    is_student = models.BooleanField(default=True)
+    
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    roll_no = models.IntegerField()
+    college_id = models.CharField(max_length=10,primary_key=True)
+    cpi=models.CharField(max_length=4)
+    
+class Event(models.Model):
+    Title = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
