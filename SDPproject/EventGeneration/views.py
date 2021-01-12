cc1304ace2cd9ae0f3bff99697fbc95d9fc0d0b6
Dev1561 <pdev0392@gmail.com>
2021-01-12 17:21:41 +0530

    events added

--- SDPproject/EventGeneration/views.py
+++ SDPproject/EventGeneration/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Student, Event, User
 
 # Create your views here.
+def Students(request):
+    # spu = User.objects.get(username="jay")
+    # spu.is_student = False
+    # spu.save(update_fields=['is_student'])
+    event_data = Event.objects.all()
+    return render(request, 'show_events.html', {'event_data':event_data})
+
+def Faculty(request):
+    event_data = Event.objects.all()
+    return render(request, 'show_events.html', {'event_data':event_data})
