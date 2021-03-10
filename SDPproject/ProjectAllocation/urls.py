6aeaea3abd7da6bed54d6c381f59a0434522c655
Jay <modi@Jays-MacBook-Air.local>
2021-03-10 20:39:05 +0530

    minor changes

--- SDPproject/ProjectAllocation/urls.py
+++ SDPproject/ProjectAllocation/urls.py
@@ -5,11 +5,7 @@ urlpatterns = [
     #path('create_team', views.CreateTeam),
     path('my_assignment/<int:pk>/validate_team', views.validate_team),
     path('team_created', views.team_created),
-<<<<<<< HEAD
     path('my_assignment/<int:pk>/team_list', views.team_list),
-=======
-    path('team_list/<int:pk>', views.team_list),
->>>>>>> 31c38185ec1f4c293ee8d09aeea96d7b0e8e8183
     path('guide_requests', views.guide_requests),
     path('guide_request/<int:pk>', views.process_request),
     path('my_assignment/<int:pk>/own_project', views.own_project),
--- SDPproject/ProjectAllocation/views.py
+++ SDPproject/ProjectAllocation/views.py
@@ -132,17 +132,12 @@ def team_created(request):
     return render(request, 'team_created.html')
 
 def team_list(request,pk):
-<<<<<<< HEAD
     event = event_models.Event.objects.get(pk=pk)
     is_head = False
     if event.event_head.user.username == str(request.user):
         is_head = True
     team_data = Team.objects.filter(event=event)
     return render(request, 'team_list.html', {'team_data':team_data, 'is_head':is_head} )
-=======
-    team_data = Team.objects.all()
-    return render(request, 'team_list.html', {'team_data':team_data, 'pk':pk} )
->>>>>>> 31c38185ec1f4c293ee8d09aeea96d7b0e8e8183
 
 def own_project(request,pk):
     if(request.method == 'POST'):
